742fe86562bcd09fc7989fa2e3d49c1bb4d383f1
Matt Miller <mattmiller85@gmail.com>
2017-05-25 14:26:35 -0400

    test

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import { InfotainmentPiServer } from './infotainment-pi-server';
 import { Server } from "ws";
-import { MessageReader } from "../../infotainment-pi-core/core";
+import { MessageReader, MessageType } from "../../infotainment-pi-core/core";
 
 let server = new InfotainmentPiServer(new Server({ port: 12345 }), new MessageReader());
+server.message.subscribe(msg => {
+    if(msg.message.type == MessageType.askForAllTiles){
+        //server.sendMessage(new )
+    }
+});
