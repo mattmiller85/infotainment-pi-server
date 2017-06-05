fffa65b677004e58a78d3a53cdb41d699eda41d8
mattmiller85 <mattmiller85@gmail.com>
2017-06-05 16:09:47 -0400

    ngfor not working

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,32 @@
+import { InfotainmentPiRepository } from './infotainment-pi-repository';
 import { InfotainmentPiServer } from './infotainment-pi-server';
 import { Server } from "ws";
-import { MessageReader, MessageType, ReturnAllTilesMessage } from "../../infotainment-pi-core/core";
+import { MessageReader, MessageType, ReturnAllTilesMessage, SingleAudioFileTile } from "../../infotainment-pi-core/core";
 
+let repo = new InfotainmentPiRepository();
 let server = new InfotainmentPiServer(new Server({ port: 12345 }), new MessageReader());
-console.log("test");
+console.log("Listening on port 12345...");
 server.message.subscribe(msg => {
     if(msg.message.type == MessageType.askForAllTiles){
-        console.log(msg);
-        server.sendMessage(new ReturnAllTilesMessage(), msg.who);
+        repo.getTiles().then((tiles) => {
+            server.sendMessage(new ReturnAllTilesMessage(tiles), msg.who);
+        });
     }
 });
+
+// repo.getTiles().then((tiles) => { 
+//     console.log(tiles) 
+// }).catch((reason) => {
+//     console.log(reason);
+// });
+
+// repo.getNextId().then((next_id) => { 
+//     console.log(next_id);
+//     let newTile = new SingleAudioFileTile();
+//     newTile.description = 'Testing description';
+//     newTile.name = "test tile name";
+//     newTile.path_to_audio = "/path/to/audio.mp3";
+//     repo.addTile(next_id, newTile);
+// }).catch((reason) => {
+//     console.log(reason);
+// });
--- src/infotainment-pi-repository.ts
+++ src/infotainment-pi-repository.ts
@@ -1,3 +1,4 @@
+import { tryCatch } from 'rxjs/util/tryCatch';
 import * as redis from 'redis';
 import { TileBase } from "../../infotainment-pi-core/core"
 
@@ -8,13 +9,41 @@ export class InfotainmentPiRepository{
         this._client = redis.createClient(6379, "localhost");    
     }
 
-    addTile(tileId: number, tile: TileBase){
-        this._client.hset("tiles", tile);
+    addTile(tileId: number, tile: TileBase): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            try{
+                this._client.set(`tile:${tileId}`, JSON.stringify(tile), (err: any, id: any) => { resolve(true) })
+            }catch(ex){
+                reject(ex);
+            }
+        });
     }
 
-    getTiles(){
-        return this._client.get("tiles", (tiles) => {
-            console.log(tiles);
+    getNextId(): Promise<number> {
+        return new Promise<number>((resolve, reject) => {
+            try{
+                this._client.incr("next_tile_id", (err:any, id: any) => { resolve(id) })
+            }catch(ex){
+                reject(ex);
+            }
+        }); 
+    }
+
+    getTiles(): Promise<TileBase[]> {
+        return new Promise<TileBase[]>((resolve, reject) => {
+            try{
+                this._client.keys("tile:*", (err:any, keys:any) => {
+                    this._client.mget(keys, (err:any, tiles: any) => {
+                        for(let i=0; i<keys.length; i++){
+                            tiles[i] = JSON.parse(tiles[i]);
+                            tiles[i].id = parseInt(keys[i].substring(5))
+                        }
+                        resolve(tiles);
+                    })
+                });
+            }catch(ex){
+                reject(ex);
+            }
         });
     }
 }
